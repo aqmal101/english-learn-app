9becfdd7055c8f57abcaed6dd47fa0aafed4f4ec
ipincamp <support@nur-arifin.my.id>
2025-05-12 23:33:56 +0700

    feat: Add .env.example for environment variables; update allowedHosts in vite.config.js
    easy for production deployment

--- vite.config.js
+++ vite.config.js
@@ -7,6 +7,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     port: 5000,
-    allowedHosts: [],
+    allowedHosts: ["MAIN_APP_URL"], // *Change This
   },
 });
